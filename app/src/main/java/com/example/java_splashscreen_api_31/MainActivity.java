0ae28c061e7b54663473b2a41947bf64d377d774
Mik0ru <aziztermecikov5@gmail.com>
2024-09-30 00:49:12 +0600

    Initial commit

---
+++ app/src/main/java/com/example/java_splashscreen_api_31/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.java_splashscreen_api_31;
+
+import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.java_splashscreen_api_31.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding viewBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        SplashScreen.installSplashScreen(this);
+        EdgeToEdge.enable(this);
+        setContentView(viewBinding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        viewBinding.lottie.playAnimation();
+    }
+}
